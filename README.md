9ff2a29b46f2424abc84a6e47276313329d249a6
Adonai14x <alanchris64@gmail.com>
2025-08-22 21:10:51 -0500

    Add initial project files including README, app, EDA notebook, and requirements

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # streamdash-v3
+# Car Sales Dashboard (Streamlit)
+
+A Streamlit dashboard built around `vehicles_us.csv` car listings dataset.  
+Includes interactive filters plus a histogram and scatter plot.
---
+++ app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import pandas as pd
+import plotly.express.px
+
+set.set_page_config(page_title="Data Dashboard", layout="wide")
+st.header("Data Dashboard")
+
+#Load Data
+df = pd.read_csv("D:\.MAGUS ARCHIEVE\.Main Projects\Workshop\streamdash-v3\vehicles_us.csv")
