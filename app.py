4658ab5af7d300641cfa30ee5a0c887719df74ea
Adonai14x <alanchris64@gmail.com>
2025-08-23 13:55:28 -0500

    Refactor app.py to use relative file path for CSV and correct import alias for pandas in EDA notebook

--- app.py
+++ app.py
@@ -3,11 +3,11 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
-set.set_page_config(page_title="Data Dashboard", layout="wide")
+st.set_page_config(page_title="Data Dashboard", layout="wide")
 st.header("Data Dashboard")
 
 #Load Data
-df = pd.read_csv("D:\.MAGUS ARCHIEVE\.Main Projects\Workshop\streamdash-v3\vehicles_us.csv")
+df = pd.read_csv("vehicles_us.csv")
 
 st.write("Car Data Dashboard", df.head())
 
