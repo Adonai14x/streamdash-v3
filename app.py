24ff8b54049c289b54a4201092b1606bd279c04c
Adonai14x <alanchris64@gmail.com>
2025-08-23 13:44:43 -0500

    Add data visualization features to app.py and enhance EDA notebook with initial analysis code

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from numpy import histogram
 import streamlit as st
 import pandas as pd
 import plotly.express as px
@@ -7,3 +8,17 @@ st.header("Data Dashboard")
 
 #Load Data
 df = pd.read_csv("D:\.MAGUS ARCHIEVE\.Main Projects\Workshop\streamdash-v3\vehicles_us.csv")
+
+st.write("Car Data Dashboard", df.head())
+
+fig_hist = px.histogram(df, x="year", y="price", color="brand", title="Car Price Distribution by Year")
+st.plotly_chart(fig_hist)
+
+fig_bar = px.bar(df, x="brand", y="price", color="brand", title="Car Price Distribution by Brand")
+st.plotly_chart(fig_bar)
+
+fig_scatter = px.scatter(df, x="year", y="price", color="brand", title="Car Price Distribution by Year")
+st.plotly_chart(fig_scatter)
+
+if st.checkbox("Show Raw Data"):
+    st.dataframe(df)
